34ce93133381ba190cc33f31ab9719cd14339114
Akhil6898 <skillcentral.dev@gmail.com>
2024-08-13 18:32:58 +0530

    IMP-6 fix: removed the section12 component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,20 @@
-"use client";
-import IndexPageView from "pages-sections/landing/page-view";
-import Header from "components/header/header";
-import Furniture from "pages-sections/furniture-1/page-view/furniture-1";
-import Furniture2 from "pages-sections/furniture-2/page-view/furniture-2";
-import { Footer1, Footer2, Footer3, Footer4 } from "components/footer";
-import { useEffect, useState } from "react";
-import { Navbar } from "components/navbar";
-import Section12 from "pages-sections/furniture-1/section-12";
-import './app.css';
+  "use client"
+  import React, { useRef } from "react";
+  import Header from "components/header/header";
+  import Furniture from "pages-sections/furniture-1/page-view/furniture-1";
+  import Section12 from "pages-sections/furniture-1/section-12";
+  import { Footer1 } from "components/footer";
+  import { Navbar } from "components/navbar";
+  import './app.css';
 
-export default function IndexPage() {
+  export default function IndexPage() {
 
     return (
-    <>
-      <Header />
-      <Navbar />
-      <Furniture  />
-      <Section12 id="contact"></Section12>
-      <Footer1 />
-    </>
-  );
-}
+      <>
+        <Header />
+        <Navbar />
+        <Furniture />
+        <Footer1 />
+      </>
+    );
+  }
